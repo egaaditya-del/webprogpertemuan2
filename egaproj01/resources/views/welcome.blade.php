8fa16f2875ee846dfa40f9e736975bdf0e65d2da
egaaditya-del <egapramandana88@gmail.com>
2025-10-14 12:20:38 +0800

    update

--- egaproj01/resources/views/welcome.blade.php
+++ egaproj01/resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
-<html lang:en>
-    <head>
-        <h3> wazzaapp <h3>
+<div class="container">
+
+<a href="./">Home</a>
+<a href="./teman">Teman</a>
+
+</div>
+
+<div class="container">
+    <h3>Data Teman</h3>
+
+</div>
--- egaproj01/routes/web.php
+++ egaproj01/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\temanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/teman',[temanController::class,'index']) ->name('sahabatselamanya');
